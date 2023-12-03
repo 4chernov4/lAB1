15e0c1afd6dd5fa064ea7a10e4b15f1d752580b7
4chernov4 <yarchernov@gmail.com>
2023-12-03 22:15:35 +0300

    10

---
+++ 3.py
@@ -0,0 +1,7 @@
+number = int(input("Введите целое число: "))
+
+next_number = number + 1
+print("The next number for the number", number, "is", next_number, end=".")
+
+previous_number = number - 1
+print("The next number for the number", number, "is", previous_number, end=".")
---
+++ 7.py
@@ -0,0 +1,5 @@
+n = int(input("Введите натуральное число: "))
+
+last_digit = n % 10
+
+print("Последняя цифра:", last_digit)
