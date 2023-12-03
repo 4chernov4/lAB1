b0c250560fc31e448c3da569d0f6eb7eb86cf088
4chernov4 <yarchernov@gmail.com>
2023-12-03 21:22:22 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+number1 = int(input("Введите первое число: "))
+number2 = int(input("Введите второе число: "))
+number3 = int(input("Введите третье число: "))
+
+sum = number1 + number2 + number3
+
+print("Сумма введенных чисел:", sum)
